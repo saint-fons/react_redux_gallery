6ad058b70a85ac28245be1a33dc6c5041541f1c7
Ivan Zhuravlev <i.zhuravlev.wok@gmail.com>
2021-02-15 09:23:23 +0300

    dark mode added

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,17 +1,58 @@
 import React from "react";
+import {ThemeProvider} from 'styled-components';
+import {lightTheme, darkTheme} from './../style/theme';
+import {GlobalStyles} from './../style/global';
+import {useState} from 'react';
+import star from "./../style/img/star-solid.svg"
 
 
 const Gallery = (props) => {
+
+    const [theme, setTheme] = useState('light');
+    const toggleTheme = () => {
+        if (theme === 'light') {
+            setTheme('dark');
+        } else {
+            setTheme('light');
+        }
+    }
+
     return (
-        <div className={"Gallery_Container_Element1"}>
-            {props.countries.map(i => (
-                <img
-                    className={"Gallery_Images_Container"}
-                    src={i.url}
-                    alt={"img"}
-                />
-            ))}
-        </div>
+
+        <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+            <>
+                <GlobalStyles/>
+
+                <div className={"Gallery_Container_Element1"}>
+                    <button
+                        onClick={toggleTheme}>
+                        {theme === 'light' ?
+                            <img
+                                src={star}
+                                alt={"star"}
+                                className={"DarkModeStar"}/>
+                            :
+                            <img
+                                src={star}
+                                alt={"star"}
+                                className={"DarkModeStar"}/>
+                        }
+                    </button>
+                    {props.countries.map(i => (
+                        <a href={i.url}>
+                            <img
+                                className={"Gallery_Images_Container"}
+                                src={i.url}
+                                alt={"img"}
+                            />
+                        </a>
+
+                    ))}
+                </div>
+
+
+            </>
+        </ThemeProvider>
     )
 }
 
---
+++ src/style/theme.js
@@ -0,0 +1,18 @@
+// theme.js
+import styled from "styled-components";
+
+export const lightTheme = {
+    body: '#ffffff',
+    text: '#363537',
+    toggleBorder: '#FFF',
+    gradient: 'linear-gradient(#39598A, #79D7ED)',
+}
+
+export const darkTheme = {
+    body: '#363537',
+    toggleBorder: '#6B8096',
+    gradient: 'linear-gradient(#091236, #1E215D)',
+}
+
+export const Title = styled.h1`
+    `;
